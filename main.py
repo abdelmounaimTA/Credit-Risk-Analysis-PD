8ae09fb322fdb2391db02a2d92a4b99f143fd74d
abdelmounaimTA <abdelmounaimtarniche@gmail.com>
2025-07-22 11:55:48 +0100

    Progress

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ df = pd.read_csv("GiveMeSomeCredit/cs-training.csv")
 
 print(df.info())
 print("*"*50)
-print(df.describe())
+
+
+df2 = pd.read_csv("GiveMeSomeCredit/cs-test.csv")
+
+print(df2.info())
+
+
